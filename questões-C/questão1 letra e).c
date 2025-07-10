269d504fe54fb3f7120f3b99ce3ddf77d4e08be7
Rafaela <rafaela12344oliveira@gmail.com>
2025-07-10 12:08:11 -0300

    Create questão1 letra e).c

---
+++ questões-C/questão1 letra e).c
@@ -0,0 +1,16 @@
+include <stdio.h>
+  int main() {
+    //declaracao de variavel 
+    float fahrenheit, celsius;
+
+    //entrada de dados
+    escreva("digite a temperatura em Fahrenheit: ");
+    printf("%f", &fahrenheit);
+
+    //processamento 
+    celsius = ((fahrenheit - 32)*5)/9;
+
+    //Saida de dados
+  printf("A temperatura em celsius é: %.f", celsius)
+    return 0;
+}

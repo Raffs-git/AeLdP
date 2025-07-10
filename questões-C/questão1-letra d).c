288ad26119fe960ac2958e7d8ff199f76df88a98
Rafaela <rafaela12344oliveira@gmail.com>
2025-07-10 11:50:40 -0300

    Create questão1-letra d).c

---
+++ questões-C/questão1-letra d).c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    //Declaração de Variáveis
+    float c , f;
+    //Entrada de dados 
+    printf("Informe o valor da temperaura em Celsius: ");
+    scanf ("%f",&c);
+    //Processamento 
+    f= (9*c+160)/5;
+    //Saída de dados
+    printf ("O valor da temperatura em Fahrenheit é: %f",&f);
+
+    return 0;
+}

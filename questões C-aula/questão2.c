c049fe3eae75b56749161288aefe01083978ab7f
Rafaela <rafaela12344oliveira@gmail.com>
2025-07-17 11:32:15 -0300

    Create questão2.c

---
+++ questões C-aula/questão2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+printf("2 + 3\t = \t 5\n");
+printf("2 - 3\t = \t-1\n");
+printf("2 * 3\t = \t 6\n");
+printf("2 / 3\t = \t 0\n");
+return 0;
+}

6bfbfc0cabc5eb265122e9857c53c5aea7012132
Rafaela <rafaela12344oliveira@gmail.com>
2025-07-17 11:23:48 -0300

    Create questão1.c

---
+++ questões C-aula/questão1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("total\t=\t100\% \n");
+    printf("IVA \t= \t17\% \n");
+    printf("IRS \t= \t15\% \n");
+    printf("-------------------\n");
+    printf("líq. \t= \t68\% \n");
+        return 0;
+}

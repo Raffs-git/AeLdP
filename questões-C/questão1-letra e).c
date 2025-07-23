c9da81cc654a117aea08ad10ff62b95aae630a59
Rafaela <rafaela12344oliveira@gmail.com>
2025-07-23 11:06:53 -0300

    Update questão1-letra e).c

--- questões-C/questão1-letra e).c
+++ questões-C/questão1-letra e).c
@@ -4,13 +4,13 @@
     float fahrenheit, celsius;
 
     //entrada de dados
-    escreva("digite a temperatura em Fahrenheit: ");
-    printf("%f", &fahrenheit);
+    printf("digite a temperatura em Fahrenheit: ");
+    scanf("%f", &fahrenheit);
 
     //processamento 
     celsius = ((fahrenheit - 32)*5)/9;
 
     //Saida de dados
-  printf("A temperatura em celsius é: %.f", celsius)
+  printf("A temperatura em celsius é: %.f", celsius);
     return 0;
 }

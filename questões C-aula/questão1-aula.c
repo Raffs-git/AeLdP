c63cc88cc156e86703980af897eb7c345894e70f
Rafaela <rafaela12344oliveira@gmail.com>
2025-08-01 09:46:32 -0300

    Create questão1-aula.c

---
+++ questões C-aula/questão1-aula.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int digito1,digito2,digito3,digito4,digito5;
+    printf ("informe seu primeiro digito: ");
+    scanf ("%d",&digito1);
+    printf ("informe seu segundo digito: ");
+    scanf ("%d",&digito2);
+    printf ("informe seu terceiro digito: ");
+    scanf ("%d",&digito3);
+    printf ("informe seu quarto digito: ");
+    scanf ("%d",&digito4);
+    printf ("informe seu quinto digito: ");
+    scanf ("%d",&digito5);
+    printf("Seu número é= %d\t %d\t%d\t%d\t%d\n", digito1, digito2, digito3, digito4, digito5);
+    return 0;
+}

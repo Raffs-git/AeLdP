667bc95c2d03629512af48914e77915f47ea8e63
Rafaela <rafaela12344oliveira@gmail.com>
2025-08-07 09:40:05 -0300

    Create questão2-aula.c

---
+++ questões C-aula/questão2-aula.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    // Cabeçalho da tabela
+    printf("numero quadrado cubo\n");
+
+    printf("\t0\t0\t\t0\n");
+    printf("\t1\t1\t\t1\n");
+    printf("\t2\t4\t\t8\n");
+    printf("\t3\t9\t\t27\n");
+    printf("\t4\t16\t\t64\n");
+    printf("\t5\t25\t\t125\n");
+    printf("\t6\t36\t\t216\n");
+    printf("\t7\t49\t\t343\n");
+    printf("\t8\t64\t\t512\n");
+    printf("\t9\t81\t\t729\n");
+    printf("\t10\t100\t\t1000\n");
+
+    return 0;
+}
